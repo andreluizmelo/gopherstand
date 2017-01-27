e322e57db9b0d3986d1f414e550097ca963d217a
Andre Luizm <andreluiz.melo@outlook.com>
2017-01-26 23:18:52 -0200

    basic server to return hello go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import(
+    "fmt"
+    "net/http"
+)
+
 func main() {
-    println("hello go")
+    fmt.Println("initializing server...")
+    http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+        fmt.Println(req)
+        w.Write([]byte("Hello Go"))
+    })
+
+    http.ListenAndServe(":8000", nil)
 }
